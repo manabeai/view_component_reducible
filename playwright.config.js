170a0c108098ddf9187acda414686fc95f14b55a
manabeai <matsu.devtool@gmail.com>
2026-01-12 15:07:15 +0900

    Start redis for e2e in CI

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     headless: true
   },
   webServer: {
-    command: 'RAILS_ENV=test bin/rails server -b 127.0.0.1 -p 3000',
+    command: 'sh -c "redis-server --daemonize yes || true; RAILS_ENV=test bin/rails server -b 127.0.0.1 -p 3000"',
     cwd: 'spec/dummy',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI
