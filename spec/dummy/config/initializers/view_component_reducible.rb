98d68bec29d162cd265b8a9433ea37e313d85a7f
manabeai <matsu.devtool@gmail.com>
2026-01-12 15:08:59 +0900

    Matrix e2e across adapters

--- spec/dummy/config/initializers/view_component_reducible.rb
+++ spec/dummy/config/initializers/view_component_reducible.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 ViewComponentReducible.configure do |config|
-  config.adapter = ViewComponentReducible::Adapter::Redis
+  adapter_name = ENV.fetch("VCR_ADAPTER", "redis")
+  config.adapter = case adapter_name
+                   when "hidden_field"
+                     ViewComponentReducible::Adapter::HiddenField
+                   when "session"
+                     ViewComponentReducible::Adapter::Session
+                   else
+                     ViewComponentReducible::Adapter::Redis
+                   end
   config.secret = Rails.application.secret_key_base
   config.redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
 end
