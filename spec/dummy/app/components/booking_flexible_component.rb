dc49221c63850c65baca6e94204b79e652c35808
manabeai <matsu.devtool@gmail.com>
2026-01-25 09:36:54 +0900

    feat: enhance booking flexibility with desired state tracking and availability checks

--- spec/dummy/app/components/booking_flexible_component.rb
+++ spec/dummy/app/components/booking_flexible_component.rb
@@ -7,6 +7,9 @@ class BookingFlexibleComponent < ViewComponent::Base
     field :selected_days, default: -> { [] }
     field :selected_times, default: -> { [] }
     field :selected_staff_ids, default: -> { [] }
+    field :desired_days, default: -> { [] }
+    field :desired_times, default: -> { [] }
+    field :desired_staff_ids, default: -> { [] }
     field :available_days, default: -> { BookingFlexibleComponent.available_days }
     field :available_times, default: -> { BookingFlexibleComponent.available_times }
     field :available_staff_ids, default: -> { BookingFlexibleComponent.available_staff_ids }
@@ -19,16 +22,16 @@ class BookingFlexibleComponent < ViewComponent::Base
   def reduce(state, msg)
     case msg
     in { type: :toggle_day, payload: payload }
-      selected_days = toggle_value(state.selected_days, normalize_day(payload.day))
-      apply_availability(state.with(selected_days: selected_days))
+      desired_days = toggle_value(state.desired_days, normalize_day(payload.day))
+      apply_availability(state.with(desired_days: desired_days))
 
     in { type: :toggle_time, payload: payload }
-      selected_times = toggle_value(state.selected_times, payload.time.to_s)
-      apply_availability(state.with(selected_times: selected_times))
+      desired_times = toggle_value(state.desired_times, payload.time.to_s)
+      apply_availability(state.with(desired_times: desired_times))
 
     in { type: :toggle_staff, payload: payload }
-      selected_staff_ids = toggle_value(state.selected_staff_ids, payload.staff_id.to_i)
-      apply_availability(state.with(selected_staff_ids: selected_staff_ids))
+      desired_staff_ids = toggle_value(state.desired_staff_ids, payload.staff_id.to_i)
+      apply_availability(state.with(desired_staff_ids: desired_staff_ids))
 
     in { type: :go_next }
       return state unless ready_for_final?(state)
@@ -69,15 +72,15 @@ class BookingFlexibleComponent < ViewComponent::Base
   end
 
   def days_for_phase
-    phase_two? ? state.selected_days : state.available_days
+    phase_two? ? state.selected_days : all_days
   end
 
   def times_for_phase
-    phase_two? ? state.selected_times : state.available_times
+    phase_two? ? state.selected_times : all_times
   end
 
   def staff_for_phase
-    ids = phase_two? ? state.selected_staff_ids : state.available_staff_ids
+    ids = phase_two? ? state.selected_staff_ids : all_staff_ids
     Staff.where(id: ids).order(:name)
   end
 
@@ -95,6 +98,42 @@ class BookingFlexibleComponent < ViewComponent::Base
     "-"
   end
 
+  def all_days
+    self.class.all_days
+  end
+
+  def all_times
+    self.class.all_times
+  end
+
+  def all_staff_ids
+    self.class.all_staff_ids
+  end
+
+  def available_day?(day_key)
+    state.available_days.include?(day_key)
+  end
+
+  def available_time?(time_label)
+    state.available_times.include?(time_label)
+  end
+
+  def available_staff?(staff_id)
+    state.available_staff_ids.include?(staff_id)
+  end
+
+  def desired_day?(day_key)
+    state.desired_days.include?(day_key)
+  end
+
+  def desired_time?(time_label)
+    state.desired_times.include?(time_label)
+  end
+
+  def desired_staff?(staff_id)
+    state.desired_staff_ids.include?(staff_id)
+  end
+
   def ready_for_final?(current_state = state)
     current_state.selected_days.any? && current_state.selected_times.any? && current_state.selected_staff_ids.any?
   end
@@ -102,26 +141,19 @@ class BookingFlexibleComponent < ViewComponent::Base
   private
 
   def apply_availability(current_state)
-    days = self.class.available_days(
-      selected_times: current_state.selected_times,
-      selected_staff_ids: current_state.selected_staff_ids
-    )
-    times = self.class.available_times(
-      selected_days: current_state.selected_days,
-      selected_staff_ids: current_state.selected_staff_ids
-    )
-    staff_ids = self.class.available_staff_ids(
-      selected_days: current_state.selected_days,
-      selected_times: current_state.selected_times
+    recalc = recalculate_active(
+      desired_days: current_state.desired_days,
+      desired_times: current_state.desired_times,
+      desired_staff_ids: current_state.desired_staff_ids
     )
 
     current_state.with(
-      available_days: days,
-      available_times: times,
-      available_staff_ids: staff_ids,
-      selected_days: current_state.selected_days & days,
-      selected_times: current_state.selected_times & times,
-      selected_staff_ids: current_state.selected_staff_ids & staff_ids
+      available_days: recalc[:available_days],
+      available_times: recalc[:available_times],
+      available_staff_ids: recalc[:available_staff_ids],
+      selected_days: recalc[:active_days],
+      selected_times: recalc[:active_times],
+      selected_staff_ids: recalc[:active_staff_ids]
     )
   end
 
@@ -138,7 +170,57 @@ class BookingFlexibleComponent < ViewComponent::Base
     value.to_s
   end
 
+  def recalculate_active(desired_days:, desired_times:, desired_staff_ids:)
+    active_days = desired_days
+    active_times = desired_times
+    active_staff_ids = desired_staff_ids
+    available_days = []
+    available_times = []
+    available_staff_ids = []
+
+    2.times do
+      available_days = self.class.available_days(
+        selected_times: active_times,
+        selected_staff_ids: active_staff_ids
+      )
+      active_days = desired_days & available_days
+
+      available_times = self.class.available_times(
+        selected_days: active_days,
+        selected_staff_ids: active_staff_ids
+      )
+      active_times = desired_times & available_times
+
+      available_staff_ids = self.class.available_staff_ids(
+        selected_days: active_days,
+        selected_times: active_times
+      )
+      active_staff_ids = desired_staff_ids & available_staff_ids
+    end
+
+    {
+      active_days: active_days,
+      active_times: active_times,
+      active_staff_ids: active_staff_ids,
+      available_days: available_days,
+      available_times: available_times,
+      available_staff_ids: available_staff_ids
+    }
+  end
+
   class << self
+    def all_days
+      all_slots.map(&:day_key).compact.uniq.sort
+    end
+
+    def all_times
+      all_slots.map(&:time_label).compact.uniq.sort
+    end
+
+    def all_staff_ids
+      all_slots.map(&:staff_id).compact.uniq.sort
+    end
+
     def available_days(selected_times: [], selected_staff_ids: [])
       slots = filtered_slots(selected_times:, selected_staff_ids:)
       slots.map(&:day_key).compact.uniq.sort
@@ -156,8 +238,12 @@ class BookingFlexibleComponent < ViewComponent::Base
 
     private
 
+    def all_slots
+      TimeSlot.includes(:staff).to_a
+    end
+
     def filtered_slots(selected_days: [], selected_times: [], selected_staff_ids: [])
-      slots = TimeSlot.includes(:staff).to_a
+      slots = all_slots
 
       if selected_staff_ids.any?
         ids = selected_staff_ids.map(&:to_i)
